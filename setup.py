68788766ccc312c145f972b881f4bfd2775427e3
Boris Zubarev <bobazooba@gmail.com>
2023-10-14 18:57:31 +0400

    preprare-hotfix

--- setup.py
+++ setup.py
@@ -83,6 +83,12 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
